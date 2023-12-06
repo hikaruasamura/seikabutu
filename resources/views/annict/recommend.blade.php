d852198e7304f4dbf47dd0ff0e5149aa8f9f8a8d
hikaruasamura <qiancunhui@gmail.com>
2023-12-07 04:37:45 +0900

    not yet

--- resources/views/annict/recommend.blade.php
+++ resources/views/annict/recommend.blade.php
@@ -1,13 +1,3 @@
-<!-- resources/views/annict/recommend.blade.php -->
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Anime Recommendations</title>
-</head>
-<body>
+<x-app-layout>
     <h2>Hi {{ $name }}! Based on your love for {{ $genre }} and your watching history, we suggest watching {{ $random_anime }}.</h2>
-</body>
-</html>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AnnictContorller;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +13,6 @@ use App\Http\Controllers\AnnictContorller;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 
 Route::get('/', function () {
     return view('welcome');
@@ -29,3 +27,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
